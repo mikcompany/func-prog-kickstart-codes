071f3340e12521209b060be0ba5b816d948ffe43
Pallas Mikko <mikko.pallas@lapinamk.fi>
2020-12-22 23:13:53 +0200

    pure functions

--- 02/FunctionsArentSpecial.playground/Pages/01PureFunctions.xcplaygroundpage/Contents.swift
+++ 02/FunctionsArentSpecial.playground/Pages/01PureFunctions.xcplaygroundpage/Contents.swift
@@ -5,5 +5,46 @@ let queenOfHearts = Card(.queen,
 let fourOfSpades = Card(.four,
                         of: .spades)
 
+var result = freshDeck
+
+func make(the card: Card, riseToTheTopOfThe deck: Deck) -> Deck {
+    var deck = deck
+    if let index = deck.firstIndex(of: card) {
+        deck.remove(at: index)
+    }
+    return deck.topped(with: card)
+}
+
+let queenFirstDeck = make(the: queenOfHearts, riseToTheTopOfThe: freshDeck)
+
+let fourFirstDeck = make(the: fourOfSpades, riseToTheTopOfThe: queenFirstDeck)
+
+extension Deck {
+    func moveToTheTop(the card: Card)  ->  Deck {
+        var deck = self
+        if let index = deck.firstIndex(of: card) {
+            deck.remove(at: index)
+        }
+        return deck.topped(with: card)
+    }
+}
+
+result = freshDeck.moveToTheTop(the: queenOfHearts).moveToTheTop(the: fourOfSpades)
+
+extension Deck {
+    func changeEveryOtherCard(to newCard: Card) -> Deck {
+        var deck = Deck()
+        for (index, card) in self.enumerated() {
+            deck.append(index.isMultiple(of: 2) ? newCard : card)
+        }
+        return deck
+    }
+}
+
+result = freshDeck.changeEveryOtherCard(to: queenOfHearts)
+
+import PlaygroundSupport
+PlaygroundPage.current.setLiveView(DeckView(of: result))
+
 //: [Next](@next)
 
